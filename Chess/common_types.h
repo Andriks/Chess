77de75ebfe2924ffcc0f618f1403f7ee675716b4
Andriks <andrik_s05@ukr.net>
2015-07-10 18:30:44 +0300

    [*] common_types.cpp
    switch to QPointer

--- Chess/common_types.h
+++ Chess/common_types.h
@@ -2,6 +2,7 @@
 #define COMMON_TYPES_H
 
 #include <QObject>
+#include <QPointer>
 #include <QString>
 #include <exception>
 
@@ -32,7 +33,6 @@ struct Cell {
 };
 
 
-
 struct CellInfo {
     Cell cell_;
     QString ftype_;
@@ -48,6 +48,11 @@ struct CellInfo {
         return (cell_==rv.cell_ && ftype_==rv.ftype_ && fcolor_==rv.fcolor_);
     }
 
+};
+
+
+class ChessEx: public std::exception
+{
 };
 
 
